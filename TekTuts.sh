963b62d1430bce4aee6f30f8094d899dd918a7eb
Anuj Singh <anuj887422@users.noreply.github.com>
2017-11-16 11:37:23 +0530

    Create TekTuts.sh

---
+++ TekTuts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Hacking-Tool-Collection-By-TekTuts
+#YouTube Channel : https://www.youtube.com/channel/UCyq6eTn6JjePkyYW86Y20ZQ
+#Facebook Page : https://www.facebook.com/tektuts/
+#Author : TekTuts
+
+clear
+
+cat  <<  EOF 
+██╗  ██╗████████╗ ██████╗  ██████╗ ██╗     ███████╗
+██║  ██║╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+███████║   ██║   ██║   ██║██║   ██║██║     ███████╗
+██╔══██║   ██║   ██║   ██║██║   ██║██║     ╚════██║
+██║  ██║   ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+                                                   
+
+EOF
+
+mkdir TekTuts
+cd TekTuts
+
+
+
+echo Now all the tools used by TekTuts is in your system. Enjoy Buddy.
+clear
+ls
